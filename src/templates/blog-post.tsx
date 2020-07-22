2441c87e1f0079cc17471406f97e10d71fcb002b
Conor Lindsay <conor.lindsay@ii.co.uk>
2020-07-22 17:42:39 +0100

    ✨ FInish setup

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Blog post</h1>
+    </Layout>
+  )
+};
+
+export default BlogPost;
