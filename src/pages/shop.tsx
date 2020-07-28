8aa974e61bb4945f4068c951126d19696a2002b0
Conor Lindsay <conor.lindsay@ii.co.uk>
2020-07-28 20:48:42 +0100

    ✨ - Shop component

---
+++ src/pages/shop.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Shop: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Shop</h1>
+    </Layout>
+  )
+};
+
+export default Shop;
